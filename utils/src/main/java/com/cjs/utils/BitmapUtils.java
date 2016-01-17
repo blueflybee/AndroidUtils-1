6ff8235e2518ba18cdaba9318e4c4e3d36a352d9
changjiashuai <changjiashuai@gmail.com>
2016-01-17 22:00:00 +0800

    update BitmapUtils.java

--- utils/src/main/java/com/cjs/utils/BitmapUtils.java
+++ utils/src/main/java/com/cjs/utils/BitmapUtils.java
@@ -11,6 +11,7 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.ExifInterface;
 import android.util.Base64;
 
 import java.io.ByteArrayInputStream;
@@ -26,10 +27,156 @@ import java.io.IOException;
  */
 public class BitmapUtils {
 
+    public static int getDegress(String path){
+        int degress = 0;
+        try {
+            ExifInterface exifInterface = new ExifInterface(path);
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation){
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degress = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degress = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degress = 270;
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return degress;
+    }
+
+    /**
+     * rotate Bitmap
+     */
+    public static Bitmap rotateBitmap(Bitmap bitmap, int degress, boolean needRecycle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degress);
+        Bitmap bm = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        if (needRecycle) {
+            bitmap.recycle();
+        }
+        return bm;
+    }
+
+    /**
+     * 图片缩放-等比缩放
+     */
+    public static void compress(String sourcePath, int targetWidth, int targetHeight) {
+        compress(sourcePath, null, false, 0L, targetWidth, targetHeight);
+    }
+
+    /**
+     * 压缩图片 （sampleSize->等比压缩）使用原路径保存文件到本地
+     *
+     * @param sourcePath      源文件路径
+     * @param qualityCompress 是否要质量压缩
+     * @param maxSize         目标图片大小
+     * @param targetWidth     目标图片宽
+     * @param targetHeight    目标图片高
+     */
+    public static void compress(String sourcePath, boolean qualityCompress, long maxSize, int targetWidth, int targetHeight) {
+        compress(sourcePath, null, qualityCompress, maxSize, targetWidth, targetHeight);
+    }
+
+    /**
+     * 压缩图片 （sampleSize->等比压缩）保存文件到本地
+     *
+     * @param sourcePath      源文件路径
+     * @param targetPath      目标文件路径
+     * @param qualityCompress 是否要质量压缩
+     * @param maxSize         目标图片大小
+     * @param targetWidth     目标图片宽
+     * @param targetHeight    目标图片高
+     */
+    public static void compress(String sourcePath, String targetPath, boolean qualityCompress, long maxSize, int targetWidth, int targetHeight) {
+        Bitmap bitmap = compress(sourcePath, targetPath, true, qualityCompress, maxSize, targetWidth, targetHeight);
+        bitmap.recycle();
+    }
+
+    /**
+     * 图片缩放
+     *
+     * @param scale 缩放倍数 >1 放大， 0..1 缩放
+     */
+    public static Bitmap compressBitmap(String sourcePath, float scale) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(sourcePath, options);
+        float targetWidth = options.outWidth * scale;
+        float targetHeight = options.outHeight * scale;
+        return compressBitmap(sourcePath, targetWidth, targetHeight);
+    }
+
+    /**
+     * 图片缩放-等比缩放
+     *
+     * @return Bitmap
+     */
+    public static Bitmap compressBitmap(String sourcePath, float targetWidth, float targetHeight) {
+        return compressBitmap(sourcePath, false, 0L, targetWidth, targetHeight);
+    }
+
+    /**
+     * 压缩图片 不保存，使用源文件路径
+     */
+    public static Bitmap compressBitmap(String sourcePath, boolean qualityCompress, long maxSize, float targetWidth, float targetHeight) {
+        return compress(sourcePath, null, false, qualityCompress, maxSize, targetWidth, targetHeight);
+    }
+
+    /**
+     * 压缩图片，默认等比压缩
+     *
+     * @param sourcePath      源文件路径
+     * @param targetPath      新文件路径，null 则使用源文件路径
+     * @param isSave          是否保存到新文件
+     * @param qualityCompress 是否压缩质量
+     * @param maxSize         压缩后最大尺寸
+     * @param targetWidth     压缩后宽度
+     * @param targetHeight    压缩后高度
+     * @return Bitmap
+     */
+    public static Bitmap compress(String sourcePath, String targetPath, boolean isSave, boolean qualityCompress, long maxSize, float targetWidth, float targetHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(sourcePath, options);
+        int sourceWidth = options.outWidth;
+        int sourceHeight = options.outHeight;
+        int inSampleSize = 1;
+        while (sourceWidth / inSampleSize > targetWidth) {
+            inSampleSize++;
+        }
+        while (sourceHeight / inSampleSize > targetHeight) {
+            inSampleSize++;
+        }
+        if (inSampleSize <= 0) {
+            inSampleSize = 1;
+        }
+        options.inSampleSize = inSampleSize;
+        Bitmap bitmap = BitmapFactory.decodeFile(sourcePath, options);
+        //等比缩放
+        bitmap = compressBitmap(bitmap, false, targetWidth, targetHeight);
+        if (qualityCompress) {
+            //质量压缩
+            bitmap = compressBitmap(bitmap, true, maxSize);
+        }
+        if (isSave) {
+            String savePath = sourcePath;
+            if (!StringUtils.isEmpty(targetPath)) {
+                savePath = targetPath;
+            }
+            saveBitmapToFile(bitmap, new File(savePath));
+        }
+        return bitmap;
+    }
+
     /**
      * 等比压缩
      */
-    public static Bitmap compressBitmap(Bitmap bitmap, boolean needRecycle, int targetWidth, int targetHeight) {
+    public static Bitmap compressBitmap(Bitmap bitmap, boolean needRecycle, float targetWidth, float targetHeight) {
         int sourceWidth = bitmap.getWidth();
         int sourceHeight = bitmap.getHeight();
         float scaleWidth = (float) targetWidth / sourceWidth;
